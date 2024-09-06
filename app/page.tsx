f135b9f05fefe13dda4cffbe265d54870308a217
Naman-Jaju <namanjaju2004@gmail.com>
2024-09-07 02:57:09 +0530

    Add basic functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
+import { Poppins } from "next/font/google"
+import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-page";
+// git push -u origin
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+})
 
 export default function Home() {
   return (
-    <Button size = "lg">
-      Click Me
-    </Button>
+   <main className="flex h-full bg-sky-500 flex-col items-center justify-center">
+    <div className="space-y-6 text-center">
+      <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md",
+        font.className,
+      )}>
+        Auth
+      </h1>
+      <p className="text-lg text-white">A simple Authentication Service</p>
+      <div>
+      <LoginButton>
+      <Button variant={"secondary"} size={"lg"}>
+        Sign in
+      </Button>
+      </LoginButton>
+    </div>
+    
+    </div>
+   </main>
   );
 }
